3e45cf35d5892f09716664b41dbbee642fbab40e
Firas Abdel Ghani <firasgany7@gmail.com>
2020-11-18 09:33:57 +0200

    First Commit

---
+++ SignedDivMul/SignedDivMul/SignedDivMul.cpp
@@ -0,0 +1,10 @@
+// SignedDivMul.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+extern "C" int IntegerMulDiv(int a, int b, int* prod, int* quo, int* rem);
+int main()
+{
+}
+
